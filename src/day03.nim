584bb073c846137f536d02c199cc53dc3f895f51
Rolando Urquiza <rolurquiza+github@gmail.com>
2024-12-03 14:32:59 +0200

    Finsh day 3

---
+++ src/day03.nim
@@ -0,0 +1,27 @@
+import nre, sugar, math, sequtils, strutils
+
+const debug = false
+
+template `->` (text: string, pattern: Regex, filter: proc (m: RegexMatch): bool = nil): seq[int] =
+    collect(newSeq):
+        for match in text.findIter pattern:
+            when debug: echo $match
+            when not filter.isNil:
+                if not filter match:
+                    continue
+            match.captures.toSeq.mapIt(parseInt it.get).prod
+
+let
+    input = stdin.readAll
+    all = input -> re"mul\((\d{1,3}),(\d{1,3})\)"
+
+echo "Part 1: " & $all.sum
+
+var dont = false
+let scoped = input -> re"mul\((\d{1,3}),(\d{1,3})\)|do(?:n't)?\(\)" do (m: RegexMatch) -> bool:
+    if (let isDo = $m == "do()"; isDo) or $m == "don't()":
+        dont = not isDo
+        return false
+    not dont
+
+echo "Part 2: " & $scoped.sum
